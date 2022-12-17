ea951f4a9cca8d4af80408f106549e9902c22b19
GabrielaTatar <gabriela.tatar07@gmail.com>
2022-12-17 20:51:53 +0200

    Laborator09

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -16,7 +16,28 @@ namespace Tatar_Gabriela_Lab07_final.Data
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
+
         }
+
+        public Task<int> SaveProductAsync(Product product)
+        {
+            if (product.ID != 0)
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+
+        public Task<int> DeleteProductAsync(Product product)
+        {
+            return _database.DeleteAsync(product);
+        }
+
 
         public Task<List<ShopList>> GetShopListsAsync()
         {
@@ -28,6 +49,11 @@ namespace Tatar_Gabriela_Lab07_final.Data
             return _database.Table<ShopList>()
                             .Where(i => i.ID == id)
                             .FirstOrDefaultAsync();
+        }
+
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
         }
 
         public Task<int> SaveShopListAsync(ShopList slist)
@@ -46,5 +72,26 @@ namespace Tatar_Gabriela_Lab07_final.Data
         {
             return _database.DeleteAsync(slist);
         }
+
+        public Task<int> SaveListProductAsync(ListProduct listp)
+        {
+            if (listp.ID != 0)
+            {
+                return _database.UpdateAsync(listp);
+            }
+            else
+            {
+                return _database.InsertAsync(listp);
+            }
+        }
+        public Task<List<Product>> GetListProductsAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>(
+            "select P.ID, P.Description from Product P"
+            + " inner join ListProduct LP"
+            + " on P.ID = LP.ProductID where LP.ShopListID = ?",
+            shoplistid);
+        }
+
     }
 }
